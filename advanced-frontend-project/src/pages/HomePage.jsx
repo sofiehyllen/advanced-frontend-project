78daa9516bb7a74da5eef79a39f5553d06ad7c99
sofiehyllen <sofiehyllen@hotmail.com>
2023-11-30 12:06:07 +0100

    set up

--- advanced-frontend-project/src/pages/HomePage.jsx
+++ advanced-frontend-project/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 export default function HomePage(){
     return(
         <section>
-            <h1 className="text-3xl font-bold underline">Hello</h1>
+            <h1 className="text-3xl font-bold underline">Hello there</h1>
         </section>
     )
 }
