e77b7baf436869cd792e9331586702fe55b48904
sofiehyllen <sofiehyllen@hotmail.com>
2023-11-30 11:34:11 +0100

    init setup

---
+++ advanced-frontend-project/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './src/pages/**/*.jsx',
+        './src/components/**/*.jsx',
+        './index.html'
+      ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
