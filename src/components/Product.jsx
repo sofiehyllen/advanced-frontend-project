53dd8065a3bdf00a05e26c3e8fccec7a63d779b7
sofiehyllen <sofiehyllen@hotmail.com>
2023-12-06 13:40:41 +0100

    produkter integreret

---
+++ src/components/Product.jsx
@@ -0,0 +1,24 @@
+import { TbShoppingBagPlus } from 'react-icons/tb';
+
+export default function Product(props) {
+	const { id, imageUrl, name, description, price } = props;
+	return (
+		<div key={id} className={'product'}>
+			<img src={imageUrl} alt={`Image of ${name}`} className={'image-product'} />
+			<h3>{name}</h3>
+			<p>{description}</p>
+			<span>${price}</span>
+			<div>
+				<button
+					className='snipcart-add-item'
+					data-item-id={id}
+					data-item-image={imageUrl}
+					data-item-name={name}
+					data-item-url='/'
+					data-item-price={price}>
+					<TbShoppingBagPlus size={40} />
+				</button>
+			</div>
+		</div>
+	);
+}
--- src/pages/WebshopPage.jsx
+++ src/pages/WebshopPage.jsx
@@ -1,18 +1,18 @@
-import products from '../products.json';
+import Product from '../components/Product';
+import products from '/products.json';
 
 export default function WebshopPage() {
-	<div className={styles.grid}>
-		{products.map((product) => {
-			return (
-				<div key={product.id} className={styles.card}>
-					<h3>{product.title}</h3>
-					<p>{product.description}</p>
-					<p>${product.price}</p>
-					<p>
-						<button>Add to Cart</button>
-					</p>
+	return (
+		<section>
+			<div>
+				<div>
+					<div className={'grid'}>
+						{products.map((product, i) => (
+							<Product {...product} key={i} />
+						))}
+					</div>
 				</div>
-			);
-		})}
-	</div>;
+			</div>
+		</section>
+	);
 }
