1a1f77843fed3c2c4fa2760b615a4707d92a968f
sofiehyllen <sofiehyllen@hotmail.com>
2023-12-17 18:28:38 +0100

    rettelser

--- src/components/kodeeksempel.jsx
+++ src/components/kodeeksempel.jsx
@@ -5,15 +5,12 @@ import { webshopDatabase } from '/src/firebaseConfig.js';
 import { onSnapshot, collection } from 'firebase/firestore';
 import { IoChevronDownSharp } from 'react-icons/io5';
 
-//--- Komponent for filtreringsfunktion på webshopsiden ---//
 const FilterOptions = ({ onFetchData }) => {
-	// State hooks til at håndtere filtreret data og valgte brands
 	const [filteredData, setFilteredData] = useState([]);
 	const [selectedBrands, setSelectedBrands] = useState([]);
 	const [availableBrands, setAvailableBrands] = useState([]);
-	const [isFilterOpen, setIsFilterOpen] = useState(false); // isNavOpen er fra start falsk = Mobil nav er lukket
+	const [isFilterOpen, setIsFilterOpen] = useState(false);
 
-	// Funktion til håndtering af valg af brands
 	const handleBrandChange = (brand) => {
 		const updatedBrands = [...selectedBrands];
 		const index = updatedBrands.indexOf(brand);
@@ -27,7 +24,6 @@ const FilterOptions = ({ onFetchData }) => {
 		setSelectedBrands(updatedBrands);
 	};
 
-	// Effekt til at opdatere brands og filtreret data baseret på valgte brands
 	useEffect(() => {
 		const brandData = async () => {
 			const productsCollection = collection(webshopDatabase, 'products');
@@ -35,32 +31,26 @@ const FilterOptions = ({ onFetchData }) => {
 			const unsubscribe = onSnapshot(productsCollection, (data) => {
 				const docs = data.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
 
-				// Her hentes alle brands fra produkterne i firebase
 				const brandsSet = new Set(docs.map((element) => element.brand));
 				const brandsArray = [...brandsSet];
 
 				setAvailableBrands(brandsArray);
 
-				// Filtrer data baseret på valgte brands
 				const filteredDocs = docs.filter(
 					(element) => selectedBrands.length === 0 || selectedBrands.includes(element.brand)
 				);
 
 				setFilteredData(filteredDocs);
 
-				// Kald onFetchData med det filtrerede data
 				onFetchData(filteredDocs);
 			});
-
 			return () => unsubscribe();
 		};
-
 		brandData();
 	}, [onFetchData, selectedBrands]);
 
 	return (
 		<div>
-			{/*--- Checkbox for hvert brand baseret på de tilgængelige brands fra Firebase ---*/}
 			<div className='sticky top-0 z-40'>
 				<button
 					type='button'
@@ -71,7 +61,6 @@ const FilterOptions = ({ onFetchData }) => {
 				</button>
 			</div>
 
-			{/*--- Filtreringsmenuen på mobil ---*/}
 			<div className={` ${isFilterOpen ? 'flex' : 'hidden'} sticky top-0 z-40 `}>
 				<div className='absolute top-10 left-0 bg-white pr-5 pb-5 pt-4'>
 					<div className='flex flex-col space-y-2 w-max  '>
@@ -95,7 +84,6 @@ const FilterOptions = ({ onFetchData }) => {
 				</div>
 			</div>
 
-			{/*--- Filtreringsmenuen på tablet og laptop ---*/}
 			<div className='hidden md:block md:sticky md:top-0 md:pt-16 md:bg-white  '>
 				<h3 className='heading2 pb-2 leading-none'>Brand</h3>
 				<div className='flex flex-col space-y-1 w-max  '>
@@ -114,7 +102,6 @@ const FilterOptions = ({ onFetchData }) => {
 				<div className='h-line mt-5'></div>
 			</div>
 
-			{/*--- De filtrerede data vises her ---*/}
 			{filteredData.map((element) => (
 				<div key={element.id}></div>
 			))}
